4c13adb356e5ff5e65992f4a62a8740c3dbc085b
jqn <jqngl@icloud.com>
2020-10-21 11:06:27 +0200

    Added react native cameraroll and react native image picker

--- App/Components/Gallery/GalleryNav.js
+++ App/Components/Gallery/GalleryNav.js
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
   },
   menu: {
     paddingHorizontal: 16,
-    marginTop: StatusBar.currentHeight || 16,
+    marginTop: StatusBar.currentHeight || 8,
     paddingTop: 32,
     flexDirection: 'row',
     justifyContent: 'space-between',
